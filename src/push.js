04137aab1b926380b47970e9748532fc2674c978
Samuelson Brito <samuelsonma@gmail.com>
2018-03-15 12:15:39 -0400

    mofificações na estrutura

--- src/push.js
+++ src/push.js
@@ -1,31 +1,29 @@
 import firebase from 'firebase'
 
-let getToken = function(){
-    const messaging = firebase.messaging()
-    messaging.getToken()
-        .then((currentToken)=>{
-            if(currentToken){
-                console.log('Registrar isso para um servidor:', currentToken)
-                return currentToken
-            }
-            console.warn('Nenhum ID disponivel, solicite permissão para gerar um')
-        })
-}
-
-export default function(){
-    
-    if('serviceWorker' in navigator){
+export default function () {
+    if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./static/firebase-messaging-sw.js')
             .then(registration => {
-
                 const messaging = firebase.messaging()
 
+                let getToken = () => {
+                    messaging.getToken()
+                        .then(currentToken => {
+                            if (currentToken) {
+                                console.log('Enviar isso para um servidor:', currentToken)
+                                return currentToken
+                            }
+                            console.warn('Nenhum is disponível, solicite permissão para gerar um')
+                        });
+                }
+
                 messaging.useServiceWorker(registration)
-                messaging.requestPermission().then(()=> getToken())                    
 
-            }).catch(e => {
-                console.warn('Erro:',e)
+                messaging.requestPermission()
+                    .then(() => {
+                        getToken()
+                    })
             })
+            .catch(err => console.warn('Erro', err))
     }
-
 }
